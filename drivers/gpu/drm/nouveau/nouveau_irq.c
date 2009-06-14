704ddfd07b65a0144f6d7001bab13e391586b723
Ben Skeggs <bskeggs@redhat.com>
2009-06-14 21:35:47 +1000

    nv50: 0x10 is ILLEGAL_MTHD
    Not sure where I got 0x20 from, typo?  My other notes, and observations from
    playing with NVAx 3D definitely indicate 0x10 is correct here..

--- drivers/gpu/drm/nouveau/nouveau_irq.c
+++ drivers/gpu/drm/nouveau/nouveau_irq.c
@@ -455,12 +455,12 @@ nv50_pgraph_irq_handler(struct drm_device *dev)
 	status = nv_rd32(NV03_PGRAPH_INTR);
 	nsource = nv_rd32(NV03_PGRAPH_NSOURCE);
 
-	if (status & 0x00000020) {
+	if (status & 0x00000010) {
 		nouveau_pgraph_intr_error(dev,
 					  NV03_PGRAPH_NSOURCE_ILLEGAL_MTHD);
 
-		status &= ~0x00000020;
-		nv_wr32(NV03_PGRAPH_INTR, 0x00000020);
+		status &= ~0x00000010;
+		nv_wr32(NV03_PGRAPH_INTR, 0x00000010);
 	}
 
 	if (status & 0x00100000) {
